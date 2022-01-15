03c611df7d656a41fe0002c296c29901467d8391
Geraldo Boueres <geraldoboueres@gmail.com>
2022-01-15 12:15:26 -0400

    params

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require ("uuid")
 
@@ -13,7 +14,6 @@ app.post("/account", (request,response) => {
         return response.status(400).json({error: "Customer Already exists!"});
     }
 
-
     customers.push({
         cpf,
         name,
@@ -25,7 +25,18 @@ app.post("/account", (request,response) => {
 
 });
 
+app.get("/statement/:cpf", (request, response) =>{
+    const { cpf } = request.params;
 
+    const customer = customers.find( customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    return response.json(customer.statement);
+
+});
 
 
 app.listen(3333);
