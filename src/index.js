6cf51416c234856eb774d67a4dadb16f8097eff8
Geraldo Boueres <geraldoboueres@gmail.com>
2022-01-15 11:53:30 -0400

    customerAlreadyExists

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-const { request, response } = require('express');
-const express = require('express');
+const express = require("express");
 const { v4: uuidv4 } = require ("uuid")
 
 const app = express();
+app.use(express.json());
 
-const costumers = [];
+const customers = [];
 
 app.post("/account", (request,response) => {
-    const {cpf, name} = request.body;
-    const id = uuidv4();
-    costumers.push({
+    const { cpf, name } = request.body;
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+    if (customerAlreadyExists){
+        return response.status(400).json({error: "Customer Already exists!"});
+    }
+
+
+    customers.push({
         cpf,
         name,
-        id,
-        statement: []
+        id: uuidv4(),
+        statement: [],
     });
-
+    console.log(customers);
     return response.status(201).send();
 
-})
+});
 
 
 
-
-
-app.use(express.json());
 
 app.listen(3333);
 
