eff61bdee576f5b71bc02da4c7cdb57da46077e5
Geraldo Boueres <geraldoboueres@gmail.com>
2022-01-15 11:23:38 -0400

    FinAPI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
+const { request, response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require ("uuid")
 
 const app = express();
+
+const costumers = [];
+
+app.post("/account", (request,response) => {
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+    costumers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+
+})
+
+
+
+
+
 app.use(express.json());
+
+app.listen(3333);
+
+
+
+
+/*
 
 app.get("/courses", (request, response)=>{
     const query = request.query;
@@ -28,6 +58,7 @@ app.patch("/courses/:id", (request, response)=>{
 
 app.delete("/courses/:id", (request,response) => {
     return response.json(["Curso 6", "Curso 2", "Curso 4"]);
-});
+}); 
 
-app.listen(3333);
+*/
+
